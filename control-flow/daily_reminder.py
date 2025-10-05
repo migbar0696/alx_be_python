415da8b671d6311deb10db1d0a234b42d3bf07cf
Migbaru Belay <migbarubelaya@gmail.com>
2025-10-05 23:40:33 +0300

    Fix daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,17 +5,17 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority_variable:
     case "high":
         if time_bound == "yes":
-            print(f"Urgent Reminder:'{task_variable}' is a high priority task that must be completed today!")
+            print(f"Reminder: '{task_variable}' is a high priority task that requires immediate attention today!")
         else:
-            print(f"Important:'{task_variable}'is a high priority task.keep it at the top of your list.")
+            print(f"Note: '{task_variable}' is a high priority task. Keep it at the top of your list.")
     case "medium":
         
         if time_bound == "yes":
-            print(f"Reminder:'{task_variable}' is a medium priority task that must be completed today!")
+            print(f"Reminder: '{task_variable}' is a medium priority task that requires immediate attention today!")
         else:
-            print(f"Important:'{task_variable}'is a medium priority task.keep it at the top of your list.")
+            print(f"Note: '{task_variable}' is a medium priority task. Consider completing it this week.")
     case "low":
         if time_bound == "yes":
-            print(f"FYI:'{task_variable}' is a low priority task, but it needs to be done today.")
+            print(f"FYI: '{task_variable}' is a low priority task, but it needs to be done today.")
         else:
-            print(f"NOTE:'{task_variable}' is a low priority task.Consider completing it when you have free time.")
+            print(f"Note: '{task_variable}' is a low priority task. Consider completing it when you have free time.")
