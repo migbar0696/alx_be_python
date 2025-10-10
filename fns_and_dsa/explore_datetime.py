47cbbcd111aaae707fb41565e623cabdb87aed83
Migbaru Belay <migbarubelaya@gmail.com>
2025-10-11 02:17:39 +0300

    Formating using strftime()

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime, date, time, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    return f"Current date and time: {current_date}"
+    return f"Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}"
 
 future_variable = int(input("Enter the number of days to add to the current date: "))
 
@@ -10,3 +10,5 @@ def calculate_future_date():
     today = datetime.now().date()
     days_to_add = timedelta(days = future_variable)
     return f"Future date: {today + days_to_add}"
+print(display_current_datetime())
+print(calculate_future_date())
