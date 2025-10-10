39122af06321ac51f7f3a868b1c97578ffda2a88
Migbaru Belay <migbarubelaya@gmail.com>
2025-10-10 21:43:33 +0300

    Add the fns_and_dsa file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1, num2, operation):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Error: Division by ZERO")
+    else:
+        print("Not correct input")
