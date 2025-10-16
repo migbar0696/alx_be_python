96f739fea3088b34ceec80675927539161897447
Migbaru Belay <migbarubelaya@gmail.com>
2025-10-16 09:57:28 +0300

    Add bank_account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount():
+    def __init__(self,account_balance):
+        self.account_balance = account_balance
+    def deposit(self,amount):
+        self.account_balance += amount
+    def withdraw(self,amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            #print(f"The current balance is {self.account_balance}")
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f"The current balance is {self.account_balance}")
+        
