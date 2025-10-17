c11ee732307e50cef65431681a1e35c595a62756
Migbaru Belay <migbarubelaya@gmail.com>
2025-10-17 12:06:44 +0300

    Fix of the test file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,19 +8,19 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
     
-    def test_add_positive(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(1 , 6), 7)
         
     def test_add_negative(self):
         self.assertEqual(self.calc.add(8, -5), 3)
         
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(8, 3), 5)
         
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 8), 24)
         
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(8, 2), 4)
          
     def test_divison_by_zero(self):
