95a7237f3e1a9cc1337b4e8d171f2ba7eadee0d3
Migbaru Belay <migbarubelaya@gmail.com>
2025-10-17 20:22:06 +0300

    Another changes

--- programming_paradigm/tempCodeRunnerFile.py
+++ programming_paradigm/tempCodeRunnerFile.py
@@ -3,20 +3,20 @@ from library_management import Book, Library
 def main():
     # Setup a small library
     library = Library()
-    library.Add_Book(Book("Brave New World", "Aldous Huxley"))
-    library.Add_Book(Book("1984", "George Orwell"))
+    library.add_book(Book("Brave New World", "Aldous Huxley"))
+    library.add_book(Book("1984", "George Orwell"))
 
     # Initial list of available books
     print("Available books after setup:")
     library.list_available_books()
 
     # Simulate checking out a book
-    library.Check_out_Book("1984")
+    library.check_out_book("1984")
     print("\nAvailable books after checking out '1984':")
     library.list_available_books()
 
     # Simulate returning a book
-    library.Return_Book("1984")
+    library.return_book("1984")
     print("\nAvailable books after returning '1984':")
     library.list_available_books()
 
