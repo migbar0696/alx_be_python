6078595b7348a4be26070ecc0f6491b1b471437a
Migbaru Belay <migbarubelaya@gmail.com>
2025-10-17 20:29:09 +0300

    One more fix

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,36 +2,36 @@ class Book:
     def __init__(self, title, author ):
         self.title = title
         self.author = author
-        self.___is_checked_out = False
+        self._is_checked_out = False
         
     def check_out(self):
-        self.___is_checked_out = True
+        self._is_checked_out = True
         
     def return_book(self):
-        self.___is_checked_out = False
+        self._is_checked_out = False
     
     def is_available(self):  # it is necessary because we can't access the ___is_checked_out outside this class
-        return not (self.___is_checked_out)
+        return not (self._is_checked_out)
 
 class Library:
     def __init__(self):
-        self.___books = []      # private attribute of name _book
+        self._books = []      # private attribute of name _book
     
     
     def Add_Book(self, book):
-        self.___books.append(book)
+        self._books.append(book)
         
     def Check_out_Book(self, title): 
-        for book in self.___books:
+        for book in self._books:
             if book.title == title and book.is_available():
                 book.check_out()
             
     def Return_Book(self, title):
-        for book in self.___books:
+        for book in self._books:
             if book.title == title and not(book.is_available()):
                 book.return_book()
         
     def list_available_books(self):
-        for book in self.___books:
+        for book in self._books:
             if book.is_available():
                 print(f"{book.title} by {book.author}")
